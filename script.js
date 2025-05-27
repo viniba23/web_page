4f72759bc3558c5f604841a9894ae9c8011c76c2
viniba23 <vinibca24@gmail.com>
2025-05-27 16:39:47 +0530

    upload calculator project

--- script.js
+++ script.js
@@ -12,14 +12,15 @@ function showSection(id) {
         title: 'Portfolio Builder',
         description: `A lightweight, responsive tool designed to help users quickly build and customize personal portfolios. I developed a sample portfolio template using HTML, CSS, and JavaScript, and the source code is publicly accessible for reference and use.<br><br><a href="https://github.com/viniba23/template_design.git" target="_blank" style="color:white;">Template  Link</a>`
       },
+      spotify: {
+        title: 'Simple Web Calculator Using HTML, CSS, and JavaScript',
+        description: 'This is a simple web-based calculator designed using HTML, CSS, and JavaScript. It features a dark-themed interface with rounded buttons, a top display area, and colored operator keys for easy identification. The calculator supports basic arithmetic operations and uses the eval() function to compute the entered expressions.<br><br><a href="https://viniba23.github.io/calculator/" target="_blank" style="color:white;">Calculator  Link</a>'
+      },
       uikit: {
         title: 'Students Details Management System',
         description: 'The Student Details Management System is a web-based application built with Angular that provides an efficient way to manage student information in educational institutions.<br><br><a href="https://github.com/viniba23/develop1.git" target="_blank" style="color:white;">Project GitHub Link</a>'
-      },
-      spotify: {
-        title: 'Hummingbird Horizon',
-        description: 'This project was developed purely for learning and experimentation with CSS-based animations and 3D effects. It features a simulated animation of a bird flying over a scenic background using only HTML and CSS, without JavaScript.<br><br><a href="https://viniba23.github.io/website/" target="_blank" style="color:white;">Design  Link</a>'
       }
+      
     };
 
     document.querySelectorAll('.project-link').forEach(link => {
